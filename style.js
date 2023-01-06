a3590f20e6f31e5ab140d44758003c4cee96d790
DEBAMRITA11 <amritadeb440@gmail.com>
2023-01-07 04:57:16 +0600

    Part-1

---
+++ style.js
@@ -0,0 +1,52 @@
+//1. getElementById Method
+//const id1 = document.getElementById("id1");
+//console.log(id1); 
+
+//inner HTML(find inner part)
+//id1.innerHTML
+//console.log(id1.innerHTML);
+
+// id1.innerHTML ="Java Script HTML DOM";
+
+// document.getElementById("id1").innerHTML = "New JavaScript Text";
+
+//2. getElementByTagName Method
+// const tag1 = document.getElementsByTagName("h1")[1];
+// console.log(tag1.innerHTML);
+// tag1.innerText = "Java-script Header Two";
+
+//3. getElementByClassName
+// const class1 = document.getElementsByClassName("class1")[0];
+// console.log(class1.innerText);
+// class1.innerText = "JavaScript Text!";
+
+//4. Dom Element Style
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// id1.style.color = "pink";
+// id1.style.backgroundColor ="green";
+// id1.style.fontSize ="50px";
+
+//5. DOM Element Attributes
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+
+// console.log(id1.attributes);
+// console.log(id1.attributes.length);
+
+//6. getAttribute ( we can find any class & Id's attribute)
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// id1.getAttribute("class");
+// console.log(id1.getAttribute("class"));
+// console.log(id1.getAttribute("id"));
+
+//7. removeAttribute (remove any attribute)
+// const id1 = document.getElementById("id1");
+// console.log(id1.innerText);
+// id1.removeAttribute("class");
+
+//8. setAttribute( add any attrribute)
+// const id2 = document.getElementById("id2");
+// console.log(id2.innerText);
+// id2.setAttribute("class", "class2");
